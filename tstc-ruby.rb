161d5143bf0243da64e524896dd9c130cd87ef1f
Frank Kair <frankkair@gmail.com>
2017-09-25 22:14:09 -0300

    Adds Transformer module

--- tstc-ruby.rb
+++ tstc-ruby.rb
@@ -63,9 +63,9 @@ class Parser
     ast
   end
 
+  private
   def walk
     token = @tokens[@count]
-
     if token[:type] == 'number'
       @count += 1
       return { type: 'NumberLiteral', value: token[:value] }
@@ -79,8 +79,7 @@ class Parser
     if token[:type] == 'paren' && token[:value] == '('
       @count += 1
       token = @tokens[@count]
-
-      node = { type: 'CallExpression', value: token[:value], params: [] }
+      node = { type: 'CallExpression', name: token[:value], params: [] }
       @count += 1
       token = @tokens[@count]
 
@@ -95,6 +94,65 @@ class Parser
   end
 end
 
+module Transformer
+  extend self
+  def transform(ast)
+    new_ast = { type: 'Program', body: [] }
+    ast[:context] = new_ast[:body]
+    traverse(ast)
+    new_ast
+  end
+
+  private
+  def traverse(ast)
+    traverse_node(ast, {})
+  end
+
+  def traverse_node(node, parent_node)
+    visitor(node, parent_node)
+    case node[:type]
+    when 'Program'
+      traverse_array(node[:body], node)
+    when 'CallExpression'
+      traverse_array(node[:params], node)
+    when 'NumberLiteral'
+    else
+      raise 'Token not recognized.'
+    end
+  end
+
+  def visitor(node, parent_node)
+    node_type = node[:type]
+    case node_type
+    when 'NumberLiteral'
+      parent_node[:context].push({ type: 'NumberLiteral', value: node[:value] })
+    when 'CallExpression'
+      expression = {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: node[:name]
+        },
+        arguments: []
+      }
+      node[:context] = expression[:arguments]
+      if parent_node[:type] != 'CallExpression'
+        expression_statement = { type: 'ExpressionStatement', expression: expression }
+        parent_node[:context].push(expression_statement)
+      else
+        parent_node[:context].push(expression)
+      end
+    end
+  end
+
+  def traverse_array(nodes, parent_node)
+    nodes.each do |node|
+      traverse_node(node, parent_node)
+    end
+  end
+end
+
 input = "(add 2 (subtract 4 2))"
 tokens = tokenizer(input)
 ast = Parser.new(tokens).parse
+new_ast = Transformer.transform(ast)
